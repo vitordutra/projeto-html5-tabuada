741113ddc2d6dff1b2c86bab9e1cf5b8431a4f23
João Vitor Dutra <jo.vitor.d@gmail.com>
2020-04-28 17:02:05 -0300

    Adicionando Comentários ao Código

--- assets/js/mult-table.js
+++ assets/js/mult-table.js
@@ -1,7 +1,10 @@
 function calcMultTable() {
+  // Obtendo do DOM o tbody da tabela
   let multTable = document.querySelector("#mult-table tbody");
+  // Obtendo o valor A do campo input number e convertendo em inteiro.
   let valorA = parseInt(document.querySelector("#valorA").value);
 
+  // Limpando o conteúdo do tbody
   multTable.innerHTML = "";
 
   for (let valorB = 0; valorB <= 10; valorB++) {
@@ -12,8 +15,11 @@ function calcMultTable() {
         <td>=</td>
         <td>${valorA * valorB}</td>
         `;
+
+    // Criando o elemento tr
     let tr = document.createElement("tr");
 
+    // Inserindo a linha na tabela
     tr.innerHTML = template;
     multTable.append(tr);
   }
@@ -21,4 +27,5 @@ function calcMultTable() {
 
 calcMultTable();
 
+// Adicionando o Evento de Alteração ao Campo Número
 document.querySelector("#valorA").addEventListener("change", calcMultTable);
